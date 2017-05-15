e17252730fb47ebf936c5d9bac3268479003f5b3
Ray Roman <rroman681@ucla.edu>
2017-05-14 18:23:17 -0700

    Include PropTypes

---
+++ app/components/CharacterItem.js
@@ -0,0 +1,38 @@
+/**
+ * Created by rroman681 on 5/11/17.
+ */
+
+import {Component} from "react";
+import PropTypes from "prop-types";
+
+export default class CharacterItem extends Component {
+    render() {
+        return (
+            <article>
+                <h1>{
+                this.state.showTraditional ?
+                    this.props.charTrad :
+                    this.props.charSimp
+                }</h1>
+                <section>
+                    <section>
+                        <h2>{this.props.keyword}</h2>
+                        <div>Lesson {this.props.lessonNumber} ({this.props.absoluteNumber})</div>
+                    </section>
+                    <section>
+                        <h2>{this.props.pinyin}</h2>
+                    </section>
+                </section>
+            </article>
+        )
+    }
+}
+
+CharacterItem.propTypes = {
+    charTrad: PropTypes.string.isRequired,
+    charSimp: PropTypes.string.isRequired,
+    lessonNumber: PropTypes.number.isRequired,
+    heisigNumber: PropTypes.number.isRequired,
+    absoluteNumber: PropTypes.number.isRequired,
+    keyword: PropTypes.string.isRequired,
+};
