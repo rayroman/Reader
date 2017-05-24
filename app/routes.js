511cf5eb9b354da27e7967ca8ee6b2cc218124e0
Ray Roman <rroman681@ucla.edu>
2017-05-23 22:14:23 -0700

    Begin architecting routes

---
+++ app/routes.js
@@ -0,0 +1,23 @@
+/**
+ * Created by rroman681 on 5/23/17.
+ */
+import React from "react";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+// Route configuration: will add more as necessary. For now, just need testing and searching
+const routes = [
+    {
+        path: "/",
+        exact: true,
+        component: null // Todo: fill in later
+    },
+    {
+        path: "/study",
+        component: null // Todo: fill in later
+    },
+    {
+        path: "/search",
+        component: null
+    }
+];
+
